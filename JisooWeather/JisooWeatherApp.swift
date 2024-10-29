802a1a5277c3ebd1bb629ac77f1443706e8583e9
Rob Ranf <rob@emiyaconsulting.com>
2024-10-28 20:06:54 -0700

    Initial Commit

---
+++ JisooWeather/JisooWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  JisooWeatherApp.swift
+//  JisooWeather
+//
+//  Created by Rob Ranf on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct JisooWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JisooWeatherTests/JisooWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  JisooWeatherTests.swift
+//  JisooWeatherTests
+//
+//  Created by Rob Ranf on 10/28/24.
+//
+
+import Testing
+@testable import JisooWeather
+
+struct JisooWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
